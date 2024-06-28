8f328f8f36aa19247b870ccb08ccc7ea06c526df
accureter <accureter@gmail.com>
2024-06-28 22:19:19 +0200

    chore: Refactor TodoApp component and add login functionality
    This commit refactors the TodoApp component in the src/components/todo/TodoApp.jsx file. It adds a login functionality to the application by introducing a LoginComponent and a WelcomeComponent. The LoginComponent allows users to enter their username and password, and upon successful authentication, displays a success message. If the authentication fails, an error message is shown. This change enhances the user experience and improves the overall functionality of the application.

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { useState } from 'react'
+import './TodoApp.css'
 
-function TodoApp() {
+export default function TodoApp() {
   return (
     <div className='TodoApp'>
-        Todo Management Application
+      Todo Management Application
+      <LoginComponent />
+      {/* <WelcomeComponent /> */}
     </div>
   )
 }
 
-export default TodoApp
+function LoginComponent() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [showFailedMessage, setShowFailedMessage] = useState(false)
+
+  function handleSubmit() {
+      if (username==='accureter' && password === 'password') {
+        setShowSuccessMessage(true)
+        setShowFailedMessage(false)
+      }else {
+        setShowFailedMessage(true)
+        setShowSuccessMessage(false)
+      }
+  }
+
+  return(
+    <div className='Login'>
+      {showSuccessMessage && <div className='successMessage'>Authenticated Sucessfully</div>}
+      {showFailedMessage && <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>}
+      <div className='LoginForm'>
+
+        <div>
+          <label>User Name</label>
+          <input type='text' name='username' placeholder='Username' value={username} onChange={(event) => setUsername(event.target.value)} />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input type='password' name='password' placeholder='Password' value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+
+        <div>
+          <button type='button' name='login' onClick={handleSubmit}>Login</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+function WelcomeComponent() {
+  return(
+    <div className='Welcome'>
+      Welcome Component
+    </div>
+  )
+}
