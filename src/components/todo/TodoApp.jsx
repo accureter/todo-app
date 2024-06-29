5271da1c4c4f2e43d69e44cb7fedefab2921ef9c
accureter <accureter@gmail.com>
2024-06-29 23:29:44 +0200

    feat: Add login functionality to TodoApp component
    The TodoApp component in the src/components/todo/TodoApp.jsx file has been refactored to include a LoginComponent and a WelcomeComponent. The LoginComponent allows users to enter their username and password, and upon successful authentication, displays a success message. If the authentication fails, an error message is shown. This change enhances the user experience and improves the overall functionality of the application.

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate, useParams } from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
   return (
     <div className='TodoApp'>
-      Todo Management Application
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<LoginComponent />}></Route>
+          <Route path='/login' element={<LoginComponent />}></Route>
+          <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
+          <Route path='*' element={<ErrorComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
@@ -18,10 +24,13 @@ function LoginComponent() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
   const [showFailedMessage, setShowFailedMessage] = useState(false)
 
+  const navigate = useNavigate()
+
   function handleSubmit() {
-      if (username==='accureter' && password === 'password') {
+      if (username === 'accureter' && password === 'password') {
         setShowSuccessMessage(true)
         setShowFailedMessage(false)
+        navigate(`/welcome/${username}`)
       }else {
         setShowFailedMessage(true)
         setShowSuccessMessage(false)
@@ -30,6 +39,7 @@ function LoginComponent() {
 
   return(
     <div className='Login'>
+      <h1>Time to Login!</h1>
       {showSuccessMessage && <div className='successMessage'>Authenticated Sucessfully</div>}
       {showFailedMessage && <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>}
       <div className='LoginForm'>
@@ -54,9 +64,21 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
+
+  const { username } = useParams()
+
   return(
     <div className='Welcome'>
-      Welcome Component
+      <h1>Welcome {username}</h1>
+      <div>Welcome Component</div>
+    </div>
+  )
+}
+
+function ErrorComponent() {
+  return(
+    <div className='Error'>
+      <h1>404 Error</h1>
     </div>
   )
 }
